4c7ccf41293925eb7dec025fba504f65f774c495
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-03-01 21:40:10 -0400

    migraciones

---
+++ src/TeamPlayers.Data/Migrations/20220302012323_AddTeamPlayersModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeamPlayers.Data.Migrations
+{
+    public partial class AddTeamPlayersModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: false),
+                    Creado = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estados", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Pais = table.Column<string>(type: "varchar(3)", unicode: false, maxLength: 3, nullable: false),
+                    IdEstado = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: false),
+                    Creado = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Equipos_Estados_IdEstado",
+                        column: x => x.IdEstado,
+                        principalTable: "Estados",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jugadores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Apellido = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: false),
+                    Nacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Pasaporte = table.Column<string>(type: "varchar(9)", unicode: false, maxLength: 9, nullable: false),
+                    Direccion = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: false),
+                    Sexo = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    IdEquipo = table.Column<int>(type: "int", nullable: true),
+                    IdEstado = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: false),
+                    Creado = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jugadores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jugadores_Equipos_IdEquipo",
+                        column: x => x.IdEquipo,
+                        principalTable: "Equipos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Jugadores_Estados_IdEstado",
+                        column: x => x.IdEstado,
+                        principalTable: "Estados",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipos_IdEstado",
+                table: "Equipos",
+                column: "IdEstado");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipos_Nombre",
+                table: "Equipos",
+                column: "Nombre",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jugadores_IdEquipo",
+                table: "Jugadores",
+                column: "IdEquipo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jugadores_IdEstado",
+                table: "Jugadores",
+                column: "IdEstado");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jugadores_Pasaporte",
+                table: "Jugadores",
+                column: "Pasaporte",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jugadores");
+
+            migrationBuilder.DropTable(
+                name: "Equipos");
+
+            migrationBuilder.DropTable(
+                name: "Estados");
+        }
+    }
+}
